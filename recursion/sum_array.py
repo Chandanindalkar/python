ab909c0b6cf59cac2b37a9971364c8ac8650e636
Chandanindalkar <chandan.s.indalkar@gmail.com>
2024-05-07 17:59:06 +0530

    improved sum_array.py using slice

--- recursion/sum_array.py
+++ recursion/sum_array.py
@@ -1,15 +1,23 @@
 nums = [2,4,6,8,10]
 
-def sum_array(arr):
-    total = 0
-    copied_array = arr
-    if len(copied_array) == 0:
-        return 0
-    elif len(copied_array) == 1:
-        return copied_array[0]
-    else:
-        total += copied_array.pop(0) + sum_array(copied_array)
-        # print(total)
-        return total
+# def sum_array(arr):
+#     total = 0
+#     copied_array = arr
+#     if len(copied_array) == 0:
+#         return 0
+#     elif len(copied_array) == 1:
+#         return copied_array[0]
+#     else:
+#         total += copied_array.pop(0) + sum_array(copied_array)
+#         # print(total)
+#         return total
 
-print(sum_array(nums))
+# print(sum_array(nums))
+
+# a more efficient way
+def sum(arr):
+    if len(arr) == 0:
+        return 0
+    return arr[0] + sum(arr[1:])
+
+print(sum(nums))
