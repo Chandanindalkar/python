10350bcf7adfb8b8e482d5f8a46fb030d907c9df
Chandanindalkar <chandan.s.indalkar@gmail.com>
2024-06-27 11:42:01 +0530

    modified breadthFS to prevent infinity loop when two nodes are friends with each other

--- searching/breadth_first_search.py
+++ searching/breadth_first_search.py
@@ -7,23 +7,27 @@ graph["bob"] = ["anuj", "peggy"]
 graph["alice"] = ["peggy"]
 graph["claire"] = ["thom", "jonny"]
 graph["anuj"] = []
-graph["peggy"] = []
+graph["peggy"] = ["you"] # peggy is friends with "you", might cause infi-loop
 graph["thom"] = []
 graph["jonny"] = []
 
+searched_queue = set()
 search_queue = deque()
 
 # First degree
 search_queue += graph["you"]
+searched_queue.add("you")
 
 def mangoSeller(name):
     return True if name[-1] == 'm' else False
 
 while search_queue:
-    person = search_queue.pop()
-    if mangoSeller(person):
-        print('The Mango Seller is: ', person)
-    else:
-        search_queue += graph[person]
+    person = search_queue.popleft()
+    if (person not in searched_queue):
+        if mangoSeller(person):
+            print('The Mango Seller is: ', person)
+        else:
+            search_queue += graph[person]
+            searched_queue.add(person)
 
 # print(search_queue)
